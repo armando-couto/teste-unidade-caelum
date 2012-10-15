3f17e0e6f39fd0464426336ea4fc315b6696675c
Armando Couto <armando-couto@hotmail.com>
2012-10-15 14:48:51 -0300

    Aulas de 1 a 4.
    Signed-off-by: Armando Couto <armando-couto@hotmail.com>

---
+++ teste-de-unidade/src/br/com/caelum/leilao/test/AvaliadorTest.java
@@ -0,0 +1,92 @@
+package br.com.caelum.leilao.test;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.caelum.leilao.builder.CriadorDeLeilao;
+import br.com.caelum.leilao.dominio.Avaliador;
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Usuario;
+
+public class AvaliadorTest implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Avaliador leiloeiro;
+	private Usuario joao;
+	private Usuario jose;
+	private Usuario maria;
+
+	@Before
+	public void criaAvaliador() {
+		this.leiloeiro = new Avaliador();
+		this.joao = new Usuario("João");
+		this.jose = new Usuario("José");
+		this.maria = new Usuario("Maria");
+	}
+
+	@Test
+	public void deveEntenderLancesEmOrdemCrescente() {
+
+		Leilao leilao = new Leilao("Playstation 3 Novo");
+		leilao.propoe(new Lance(maria, 250.0));
+		leilao.propoe(new Lance(joao, 300.0));
+		leilao.propoe(new Lance(jose, 400.0));
+
+		// executando a acao
+		leiloeiro.avalia(leilao);
+
+		// comparando a saida com o esperado
+		assertEquals(400, leiloeiro.getMaiorLance(), 0.0001);
+		assertEquals(250, leiloeiro.getMenorLance(), 0.0001);
+	}
+
+	@Test
+	public void deveEntenderLancesEmOrdemCrescenteComOutrosValores() {
+
+		Leilao leilao = new Leilao("Playstation 3 Novo");
+		leilao.propoe(new Lance(maria, 1000.0));
+		leilao.propoe(new Lance(joao, 2000.0));
+		leilao.propoe(new Lance(jose, 3000.0));
+
+		leiloeiro.avalia(leilao);
+
+		assertEquals(3000, leiloeiro.getMaiorLance(), 0.0001);
+		assertEquals(1000, leiloeiro.getMenorLance(), 0.0001);
+	}
+
+	@Test
+	public void deveEntenderLeilaoComApenasUmLance() {
+
+		Leilao leilao = new Leilao("Playstation 3 Novo");
+		leilao.propoe(new Lance(joao, 1000.0));
+
+		leiloeiro.avalia(leilao);
+
+		// veja que não precisamos mais da palavra Assert!
+		assertEquals(1000, leiloeiro.getMaiorLance(), 0.0001);
+		assertEquals(1000, leiloeiro.getMenorLance(), 0.0001);
+	}
+
+	@Test
+	public void deveEncontrarOsTresMaioresLances() {
+
+		Leilao leilao = new CriadorDeLeilao().para("Playstation 3 Novo")
+				.lance(joao, 100.0)
+				.lance(maria, 200.0)
+				.lance(joao, 300.0)
+				.lance(maria, 400.0)
+				.constroi();
+
+		leiloeiro.avalia(leilao);
+
+		List<Lance> maiores = leiloeiro.getTresMaiores();
+		assertEquals(3, maiores.size());
+	}
+}
